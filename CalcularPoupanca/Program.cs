66952d96a17fc39fbd349f0ba4d4d015b8cbbd68
FabianoSPFC <programador.workspace@gmail.com>
2018-11-19 22:53:02 -0300

    Add files via upload

---
+++ CalcularPoupanca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularPoupancaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Valor_Investido = 1000;
+            int cont = 1;
+            while (cont <= 12)
+            {
+                Valor_Investido = Valor_Investido + Valor_Investido * 0.0036;
+                Console.WriteLine("Após " + cont + " mês, você tera R$ " + Valor_Investido);
+                cont++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
