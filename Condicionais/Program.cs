bb0f0675b321efd49e0f77ef955be24385d77a3e
FabianoSPFC <programador.workspace@gmail.com>
2018-11-20 14:24:34 -0300

    Add files via upload

---
+++ Condicionais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 70;
+
+                if (idade >= 65)
+                    Console.WriteLine("Vote se quiser!");
+                else if (idade >= 16)
+                    Console.WriteLine("Pode Votar!");
+                else if (idade <16)
+                    Console.WriteLine("Não pode votar!");
+                Console.ReadLine();
+            }
+        }
+    }
