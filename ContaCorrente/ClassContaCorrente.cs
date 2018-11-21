40a4a3516b1da3c00008cf4e6255b0b1fc798d3d
FabianoSPFC <programador.workspace@gmail.com>
2018-11-21 17:29:45 -0300

    Add files via upload

---
+++ ContaCorrente/ClassContaCorrente.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using ContaCorrente;
+//using ContaCorrente; ou como está feita na linha 6
+
+namespace ContaCorrente
+{
+    public class ClassContaCorrente
+    {
+        private int agencia;
+        private int numero;
+        private double saldo = 200;
+
+        public static int TotalContas { get; private set; }
+        public int Agencia {
+            get
+            {
+                return agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+        public Cliente Titular { get; set; }
+        public double Saldo {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+            }
+         }
+       
+        public ClassContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalContas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ClassContaCorrente destino)
+        {
+            if (saldo < valor)
+                return false;
+            else
+            {
+                saldo -= valor;
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
---
+++ ContaCorrente/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ContaCorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente Novo = new Cliente();
+            Novo.Nome = "Fabiano";
+            Novo.Profissao = "Programador;";
+            Novo.CPF = "xxxxxxx-xx";
+            ClassContaCorrente conta = new ClassContaCorrente(5,10);           
+            conta.Titular = Novo;            
+            
+
+            Console.WriteLine("Titular: " + Novo.Nome  + "\nAgência: "+ conta.Agencia + "\nNúmero: " + conta.Numero + "\nSaldo: " + conta.Saldo);
+
+            Cliente nome = new Cliente();
+            nome.Nome = "Jorge";
+
+            ClassContaCorrente novo = new ClassContaCorrente(8,30);
+            novo.Titular = nome;
+            novo.Sacar(50);
+
+            Console.WriteLine("\nTitular: " +Novo.Nome + "\nSaldo Anterior: " + conta.Saldo + "\nNovo Saldo: " + novo.Saldo + "\nMeu CPF é: "+ conta.Titular.CPF);
+
+            novo.Depositar(500);
+            Console.WriteLine("\nTitular: " + nome.Nome + "\nSaldo Anterior: " + conta.Saldo + "\nNovo Saldo: " + novo.Saldo);
+
+            Console.WriteLine("\n\nSaldo de Fabiano antes da trasnferência: " + conta.Saldo + "\nSaldo de Jorge antes da trasnferência: " + novo.Saldo);           
+
+            novo.Transferir(300, conta);
+            Console.WriteLine("\n\nSaldo de Fabiano depois da trasnferência: " + conta.Saldo + "\nSaldo de Jorge depois da trasnferência: " + novo.Saldo);
+            Console.WriteLine("\n\nForam criadas " + ClassContaCorrente.TotalContas + " contas");
+            Console.ReadLine();
+        }
+    }
+}
